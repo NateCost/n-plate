eab1c36601f0c9120124c0ed8364174428e1b5d8
NateCost <ilya.sakalou@gmail.com>
2021-11-19 20:01:39 +0100

    add test to success network

--- N-PlateTests/Networking/NetworkManagerTests.swift
+++ N-PlateTests/Networking/NetworkManagerTests.swift
@@ -5,19 +5,42 @@
 
 import XCTest
 @testable import N_Plate
+import SwiftUI
 
 // MARK: - Instantiation
 extension NetworkManagerTests {
   func test_execute_request_calls_execute() {
     let sut = makeSUT(apiProvider: NetworkAPIProvider())
-    let testRequest = NetworkRequestSpy()
     var completed = false
 
-    sut.execute(request: testRequest) { _ in
+    sut.execute(request: NetworkRequestSpy()) { _ in
       completed = true
     }
 
     XCTAssertTrue(completed)
+  }
+
+  func test_execute_request_success_1() {
+    let sut = makeSUT(apiProvider: NetworkAPIProvider())
+    var testResult: Result<Data, NetworkError> = .failure(.connectionError)
+
+    sut.execute(request: NetworkRequestSpy()) { result in
+      testResult = result
+    }
+
+    XCTAssertEqual(testResult, .success(Data()))
+  }
+
+  func test_execute_request_success_2() {
+      let result = Data(base64Encoded: "2121")
+      let sut = makeSUT(apiProvider: NetworkAPIProvider())
+      var testResult: Result<Data, NetworkError> = .failure(.connectionError)
+
+      sut.execute(request: NetworkRequestSpy()) { result in
+        testResult = result
+      }
+
+      XCTAssertEqual(testResult, .success(try XCTUnwrap(result)))
   }
 }
 
