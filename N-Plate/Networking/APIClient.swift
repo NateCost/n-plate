c47fbfa5f1058c3e503d0667d0299d13ff417670
Ilya Sakalou <ilya.sakalou@thetrainline.com>
2023-02-12 13:47:23 +0100

    rename APIClient add StubbedAPIClient

--- N-Plate/Networking/APIClient.swift
+++ N-Plate/Networking/APIClient.swift
@@ -1,16 +1,30 @@
 //
-//  APIProvider.swift
+//  APIClient.swift
 //  Created by Ilya Sakalou on 10/11/2021.
 //
 
 import Foundation
 
-protocol APIProvider {
+protocol APIClientType {
   associatedtype ErrorType: Error
   func execute(_ request: RequestType, completion: @escaping (Result<Data, ErrorType>) -> Void)
 }
 
-final class NetworkAPIProvider: APIProvider {
+final class APIClient: APIClientType {
+  let networkService: NetworkService
+
+  init(networkService: NetworkService) {
+    self.networkService = networkService
+  }
+
+  func execute(_ request: RequestType, completion: @escaping (Result<Data, NetworkError>) -> Void) {
+    networkService.request(urlRequest: request.urlRequest) { _, _, _ in
+
+    }
+  }
+}
+
+final class StubbedAPIClient: APIClientType {
   let networkService: NetworkService
 
   init(networkService: NetworkService) {
---
+++ N-PlateTests/Networking/APIClientTests.swift
@@ -0,0 +1,50 @@
+//
+//  APIClientTests.swift
+//  Created by Ilya SAKALOU on 12/02/2023.
+//
+
+import XCTest
+@testable import N_Plate
+
+// MARK: - Instantiation
+extension NetworkManagerTests {
+  func test_execute_calls_networkSerice() {
+    let networkServiceStub = NetworkServiceStub()
+    let sut = makeSUT(networkService: networkServiceStub)
+
+    sut.execute(NetworkRequest(path: "")) { _ in }
+
+    XCTAssertEqual(networkServiceStub.log, [.didCallRequest])
+  }
+
+  func test_stubbed_client_execute_calls_networkSerice() {
+    let networkServiceStub = NetworkServiceStub()
+    let sut = makeStubbedSUT(networkService: networkServiceStub)
+
+    sut.execute(NetworkRequest(path: "")) { _ in }
+
+    XCTAssertEqual(networkServiceStub.log, [.didCallRequest])
+  }
+}
+
+class NetworkManagerTests: XCTestCase {
+  func makeSUT(networkService: NetworkService = DefaultNetworkService()) -> APIClient {
+    APIClient(networkService: networkService)
+  }
+
+  func makeStubbedSUT(networkService: NetworkService = DefaultNetworkService()) -> StubbedAPIClient {
+    StubbedAPIClient(networkService: networkService)
+  }
+}
+
+final class NetworkServiceStub: NetworkService {
+  var log: [LoggableAction] = []
+
+  func request(urlRequest: URLRequest, completion: @escaping (Response) -> Void) {
+    log.append(.didCallRequest)
+  }
+
+  enum LoggableAction {
+    case didCallRequest
+  }
+}
